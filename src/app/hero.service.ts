710c27ff2148532f53b78f0e6aa128e8f0360e12
Gregs <gregory.gerard@wister.fr>
2017-02-22 18:04:19 +0100

    Tutorial - Part 5 - Ko

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
@@ -12,7 +15,15 @@ export class HeroService {
     });
   }
 
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
