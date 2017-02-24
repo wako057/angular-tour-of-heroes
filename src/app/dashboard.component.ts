44a95765c080564d22261edd85a9970c67cb29a3
Gregs <gregory.gerard@wister.fr>
2017-02-24 20:10:59 +0100

    Step 7 - fix start OK

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -20,10 +20,3 @@ export class DashboardComponent implements OnInit {
       .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
-
-
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,6 +19,11 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
       // Simulate server latency with 2 second delay
@@ -30,13 +35,13 @@ export class HeroService {
     return this.getHeroes()
                .then(heroes => heroes.find(hero => hero.id === id));
   }
-
-  getHeroes(): Promise<Hero[]> {
-    return this.http.get(this.heroesUrl)
-      .toPromise()
-      .then(response => response.json().data as Hero[])
-      .catch(this.handleError);
-  }
+  //
+  // getHeroes(): Promise<Hero[]> {
+  //   return this.http.get(this.heroesUrl)
+  //     .toPromise()
+  //     .then(response => response.json().data as Hero[])
+  //     .catch(this.handleError);
+  // }
 }
 
 
